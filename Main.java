e17fc59223b980fc8d6d85ddf6cf37f115d9edfa
noahvargaspollito <noahvargaspolo73@gmail.com>
2026-07-23 10:44:20 -0700

    Updated with constructors and parameters updated with testing for defualt parameters

--- Main.java
+++ Main.java
@@ -9,7 +9,9 @@ class Main
   public static void main(String[] args) 
 	{
         DACArecipient person1 = new DACArecipient("Polo", "Noah","58-4-123", " Mexico", 2455401, 2457958, 2461695, 'M' );
+        DACArecipient person2 = new DACArecipient();
         System.out.println(person1.toString());
+        System.out.println(person2.toString());
 
   }
 }
